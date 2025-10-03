b20a1b548fcdabfe50c2eeb247393cd70f37dcd5
covid2020-w1 <dwjelke@gmail.com>
2025-10-03 17:01:47 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,16 @@
+const mapData = 
+
+[
+    {
+        coordinates: [40.00, -97.00],
+        locationName: "Harvard"
+    },
+    {
+        coordinates: [30.00, -97.00],
+        locationName: "Oklahoma Health"
+    },
+    {
+        coordinates: [25.00, -97.00],
+        locationName: "Texas Hospital"
+    },
+]
